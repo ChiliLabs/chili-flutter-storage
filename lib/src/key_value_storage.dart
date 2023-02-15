95f16bd938fa1f9bd27081e5d70ef9a856429fba
Maksims Pelna <maksims.pelna@gmail.com>
2023-02-15 12:54:18 +0200

    Abstract class `KeyValueStorage`

---
+++ lib/src/key_value_storage.dart
@@ -0,0 +1,19 @@
+abstract class KeyValueStorage {
+  Future<String?> getString(String key);
+
+  Future<void> setString(String key, String value);
+
+  Future<bool?> getBool(String key);
+
+  Future<void> setBool(String key, bool value);
+
+  Future<int?> getInt(String key);
+
+  Future<void> setInt(String key, int value);
+
+  Future<void> remove(String key);
+
+  Future<Set<String>> getKeys();
+
+  Future<void> clearAll();
+}
